2821815ccd139fd587fed12f267005d69afb0e38
whalenado <98499763+whalenado@users.noreply.github.com>
2022-02-24 16:04:39 -0500

    Update script.js
    Updating JavaScript code

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+	var principal = document.getElementById("principal").value;
+	if (Number(principal) <= 0) {
+		alert("Enter a positive number");
+	} else {
+		var rate = document.getElementById("rate").value;
+		var years = document.getElementById("years").value;
+		var interest = principal * years * rate / 100;
+		var year = new Date().getFullYear() + parseInt(years);
+		document.getElementById("result").innerHTML = "<div class='output' >If you deposit " +
+			"<span class='highlight'>" + principal + "</span>" +
+			",\<br\><div class='output'>at an interest rate of " + "<span class='highlight'>" +
+			rate + "%</span>" + ".\<br\><div class='output'>You will receive an amount of " +
+			"<span class='highlight'>" + interest + "</span>" +
+			",\<br\><div class='output'>in the year " + "<span class='highlight'>" +
+			year + "</span></div>" + "\<br\>";
+	}
 }
-        
+
+function updateRate() {
+	var rateval = document.getElementById("rate").value;
+	document.getElementById("rate_val").innerText = rateval + " %";
+}
+
+function validate() {
+	var principal = document.getElementById("principal").value;
+	if (Number(principal) <= 0) {
+		alert("Enter a positive number");
+	}
+}
